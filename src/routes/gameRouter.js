2cf91279755df6a7d2f0dfc4101ffc5658823e4a
Lyloo <deeb4905@outlook.fr>
2022-11-22 18:34:56 +0100

    ajout des 2 premières routes

--- src/routes/gameRouter.js
+++ src/routes/gameRouter.js
@@ -1,5 +1,6 @@
 import express from "express"
 import * as gameService from "../services/gameService"
+import * as database from "../database/index.js"
 
 const router = express.Router()
 
@@ -7,10 +8,40 @@ const router = express.Router()
 router.post("/", function (req, res) {
   // TODO retourner le status 400 si le nom n'existe pas.
   if (!req.body.name) {
-    return res.status(400).send("un texte random")
+    return res.status(400).send("Le nom n'existe pas.")
   }
   const newGame = gameService.createGame(req.body.name)
   res.status(201).json({ id: newGame.id, name: newGame.name })
+})
+
+router.get("/", function (req, res) {
+  const games = database.getGames()
+
+  const croppedGames = games.map(game => { return {id: game.id, name : game.name}})
+  res.status(200).json(croppedGames)
+})
+
+router.get("/:gameId/players/:playerId", function(req, res) {
+  const games = database.getGames()
+  const playerId = parseInt(req.params.playerId)
+
+  const foundGame = games.find(game => game.id.toString() === req.params.gameId)
+  if(!foundGame)
+  {
+    return res.status(404).send("La partie n'existe pas" + req.params.gameId)
+  }
+  const goodgamegoodplayer = {
+    currentPlayerIndex : foundGame.currentPlayerIndex,
+    name : foundGame.name,
+    id : foundGame.id,
+    market : foundGame.market,
+    tokens : foundGame.tokens,
+    hand : foundGame._players[playerId].hand,
+    camelsCount : foundGame._players[playerId].camelsCount,
+    winnerIndex : foundGame.winnerId,
+    bonusTokens : foundGame._bonusTokens,
+  }
+  res.status(200).json(goodgamegoodplayer)
 })
 
 export default router
